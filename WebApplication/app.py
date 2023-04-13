7664a60de16500cbe4614409886633123ff60599
conradn <cnsubuga2@gmail.com>
2023-04-13 07:53:27 +0300

    some of the front end pages

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 from flask_mysqldb import MySQL
+from cart import *
+
 
 app = Flask(__name__)
 
@@ -17,6 +19,22 @@ mysql = MySQL(app)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard_page():
+    return render_template('dash.html')
+
+@app.route('/sales')
+def sales_page():
+    return render_template('sales.html')
+
+@app.route('/manageproducts')
+def manageproducts_page():
+    return render_template('manageproducts.html')
 
 
 @app.route('/api/add', methods=['POST'])
@@ -67,6 +85,29 @@ def search():
         products_list.append(product)
     return jsonify(products_list)
 
+@app.route('/api/add_to_cart', methods=['POST'])
+def add_to_cart():
+    product_id = request.form['product_id']
+    product_name = request.form['product_name']
+    product_price = request.form['product_price']
+    product_quantity = request.form['product_quantity']
+    product_profile = request.form['product_profile']
+
+    product = {'id': product_id, 'name': product_name, 'price': product_price,
+               'quantity': product_quantity, 'profile': product_profile}
+
+    Cart.get_instance().add_product(product)
+
+    return jsonify({'message': 'Added to cart'})
+
+
+@app.route('/cart')
+def cart():
+    # Sample cart data
+
+    cart = Cart.get_instance().get_products()
+    # cart_total = sum(item['price'] * item['quantity'] for item in cart)
+    return render_template('cart.html', cart=cart, cart_total=Cart.get_instance().get_total())
 
 if __name__ == "__main__":
     app.run(debug=True)
